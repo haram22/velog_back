2d7e84e4cb045471e0b4cd5fdfa626870feb0cd3
haram22 <kimhr9889@handong.ac.kr>
2024-01-21 16:58:25 +0900

    [Feat] add server project

---
+++ Velog_server/src/main/java/com/example/Velog_server/VelogServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Velog_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelogServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VelogServerApplication.class, args);
+	}
+
+}
