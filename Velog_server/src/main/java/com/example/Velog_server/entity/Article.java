48f4744b62d61ba9c8ee72614291d426c69496a3
haram22 <kimhr9889@handong.ac.kr>
2024-01-29 02:11:18 +0900

    [Fix] Camel case

--- Velog_server/src/main/java/com/example/Velog_server/entity/Article.java
+++ Velog_server/src/main/java/com/example/Velog_server/entity/Article.java
@@ -11,41 +11,41 @@ import java.util.Date;
 public class Article {
     @Id // 대표값을 지정한다. (주민번호처럼)
     @GeneratedValue // 1, 2, 3 .. 이런식으로 자동 생성해주는 어노테이션이다.
-    private Long article_id; //primary key
+    private Long articleId; //primary key
     @Column
     private String title;
     @Column
     private String content;
     @Column
-    private int like_count;
+    private int likeCount;
     @Column
-    private int comment_count;
+    private int commentCount;
     @Column
-    private Date reg_date;
+    private Date regDate;
     @Column
-    private Date mod_date;
+    private Date modDate;
 
     // entity를 위한 생성자를 만들어준다.
-    public Article(Long article_id, String title, String content, int like_count, int comment_count, Date reg_date, Date mod_date) {
-        this.article_id = article_id;
+    public Article(Long ArticleId, String title, String content, int likeCount, int commentCount, Date regDate, Date modDate) {
+        this.articleId = ArticleId;
         this.title = title;
         this.content = content;
-        this.like_count = like_count;
-        this.comment_count = comment_count;
-        this.reg_date = reg_date;
-        this.mod_date = mod_date;
+        this.likeCount = likeCount;
+        this.commentCount = commentCount;
+        this.regDate = regDate;
+        this.modDate = modDate;
     }
 
     @Override
     public String toString() {
         return "Article{" +
-                "article_id=" + article_id +
+                "article_id=" + articleId +
                 ", title='" + title + '\'' +
                 ", content='" + content + '\'' +
-                ", like_count=" + like_count +
-                ", comment_count=" + comment_count +
-                ", reg_date=" + reg_date +
-                ", mod_date=" + mod_date +
+                ", like_count=" + likeCount +
+                ", comment_count=" + commentCount +
+                ", reg_date=" + regDate +
+                ", mod_date=" + modDate +
                 '}';
     }
 }
