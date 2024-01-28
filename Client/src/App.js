ffd255f0cba9476dcecaa172a9e782d4c59884d1
haram22 <kimhr9889@handong.ac.kr>
2024-01-28 15:17:46 +0900

    [Fix] netlify 빈화면 오류 _ router에  basename 추가

--- Client/src/App.js
+++ Client/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Router />
+      <Router basename={process.env.PUBLIC_URL}/>
     </ThemeProvider>
   );
 }
